55ed0e5f64c26ed5840fee30b374ef47c4b4c0c2
noproname1998 <noproname1998@gmail.com>
2018-03-01 17:50:37 +0700

    update 1/3

--- SERVER.js
+++ SERVER.js
@@ -22,7 +22,7 @@ function getRandomColor() {
 io.on("connection",function(socket){
 	var color_username= getRandomColor();
 
-
+	var strangerid='';
 
 	var side=Math.floor((Math.random() * 2) + 1);
 	console.log(socket.id+ " vua ket noi");
@@ -35,24 +35,73 @@ io.on("connection",function(socket){
 		else
 		{
 			var rand_ava=data.gender+Math.floor((Math.random()*5)+1);
-
-			user_online_arr.push({username:data.username,gender:rand_ava});
+			//Luu thong tin vao mang user_online_array
+			user_online_arr.push({username:data.username,gender:rand_ava,id:socket.id,status:0});
 			socket.username=data.username;
-			console.log(data.gender);
+			//console.log(data.gender);
 			socket.emit("server-send-username",{username:data.username,gender:rand_ava});
-			io.sockets.emit("server-send-userarr",user_online_arr);
+			 io.sockets.emit("server-send-userarr",user_online_arr);
+			//Ket noi random 2 user (status=0 :dang cho, status =1 : dang chat)
+
+			for(var i=0;i<user_online_arr.length;i++){
+				if(user_online_arr[i].id!=socket.id&&user_online_arr[i].status==0)
+				{
+					//Trang  thai socket thanh 1
+					user_online_arr[i].status=1;
+					//Trang thai cua user thanh 1
+					for(var j=0;j<user_online_arr.length;j++){
+						if(user_online_arr[j].id==socket.id){
+							user_online_arr[j].status=1;
+							
+						}
+					}
+
+
+
+					strangerid=user_online_arr[i].id;
+					
+					break;
+				}
+			}
+			//Trương hop user dau tien se khong co strangerid
+			if(strangerid!='')
+			{
+				socket.join(strangerid);
+				io.sockets.in(strangerid).emit("server-send-stranger");	
+			}
 			
+			 socket.on("user-send-mess",function(data){
+
+			 	var currentdate = new Date().toLocaleString();
+			 		//Trong truong hop user dau tien
+			 		if(strangerid!='')
+			 		{
+			 			io.sockets.in(strangerid).emit("server-send-mess",{username:socket.username,mess:data,date:currentdate,s:side,color:color_username});
+			 				
+			 		}
+			 		else
+			 		{
+			 			io.sockets.in(socket.id).emit("server-send-mess",{username:socket.username,mess:data,date:currentdate,s:side,color:color_username});
+			 		}
+				 	
+				
+			 });
+
 		}
 	});
-	socket.on("user-send-mess",function(data){
-				var currentdate = new Date().toLocaleString();
-				io.sockets.emit("server-send-mess",{username:socket.username,mess:data,date:currentdate,s:side,color:color_username});
-			});
+	
 
 	//Xoa userkhi disconnect
 	socket.on("disconnect",function(){
 		user_online_arr.splice(user_online_arr.indexOf(socket.username),1);
 		io.sockets.emit("server-send-userarr",user_online_arr);
+		for(var i=0;i<user_online_arr.length;i++)
+		{
+						if(user_online_arr[i].id==socket.id){
+							user_online_arr[i].status=0;
+						}
+		}
+
 	});
 });
 	
